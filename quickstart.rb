c655312d5c93ceb6bb02185d45685d204e93fdb5
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-04-16 09:51:19 -0400

    fetch data from the freedom spreadsheet
    We used our own spreadsheet here to test that we could

--- quickstart.rb
+++ quickstart.rb
@@ -43,10 +43,10 @@ service.authorization = authorize
 
 # Prints the names and majors of students in a sample spreadsheet:
 # https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-spreadsheet_id = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-range = "Class Data!A2:E"
+spreadsheet_id = "18zi9WL17Z94QdLoaCIVZLtoRXtoxj8O7bXqcfkkXu0s"
+range = "Sheet1!A2:F"
 response = service.get_spreadsheet_values spreadsheet_id, range
-puts "Name, Major:"
+puts "Name, Date:"
 puts "No data found." if response.values.empty?
 response.values.each do |row|
   # Print columns A and E, which correspond to indices 0 and 4.
